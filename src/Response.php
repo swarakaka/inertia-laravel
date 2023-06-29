c7dc38bea9e9af2ce2ee82f0dd167d97ab557ef7
swarakaka <swara09@gmail.com>
2023-06-29 12:28:15 +0200

    fix redirect to intended

--- src/Response.php
+++ src/Response.php
@@ -143,7 +143,7 @@ class Response implements Responsable
                     return $response;
                 }
 
-                $url = $response->isRedirect() ? $response->getTargetUrl() : $this->basePageUrl;
+                $url = $response->isRedirect() ? $response->getTargetUrl() : null;
             } while ($url);
 
             App::instance('request', $request);
